5806eedf270ba1158e24c5b7ca019b66e91c3940
moisestech <m@moises.tech>
2021-05-31 05:30:27 -0400

    js solution comments

---
+++ Easy/validate-subsequence/solution_1/validate-subsequence.js
@@ -0,0 +1,15 @@
+// SOLUTION 1
+// O(n) time | O(1) space - where n is the length of the array
+function isValidSubsequence(array, sequence) {
+  let arrIdx = 0;
+  let seqIdx = 0;
+
+  const arrLen = array.length;
+  const seqLen = sequence.length;
+
+  while (arrIdx < arrLen && seqIdx < seqLen) {
+    if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
+    arrIdx++;
+  }
+  return seqIdx === seqLen;
+}
