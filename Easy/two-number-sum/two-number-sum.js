7ec7c45e0c4925dfc55182d0804734d0aced07c0
moisestech <m@moises.tech>
2021-05-31 04:59:42 -0400

    solution 1 O(n^2) time | O(n) space

---
+++ Easy/two-number-sum/two-number-sum.js
@@ -0,0 +1,14 @@
+// O(n^2) time | O(1) space
+function twoNumberSum(array, targetSum) {
+  const arrLen = array.length;
+  for (let i = 0; i < arrLen - 1; i++) {
+    const firstNum = array[i];
+    for (let j = i + 1; j < arrLen; j++) {
+      const secondNum = array[j];
+      if (firstNum + secondNum === targetSum) {
+        return [firstNum, secondNum];
+      }
+    }
+  }
+  return [];
+}
