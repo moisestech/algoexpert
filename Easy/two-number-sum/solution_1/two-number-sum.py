0206be4b58d896e7c98c257010091d333ab8895b
moisestech <m@moises.tech>
2021-06-03 05:04:36 -0400

    solution py

--- Easy/two-number-sum/solution_1/two-number-sum.py
+++ Easy/two-number-sum/solution_1/two-number-sum.py
@@ -1,3 +1,12 @@
 # SOLUTION 1
 # Two Number Sum
 # Complexity: O(n^2) time | O(1) space
+
+def twoNumberSum(array, targetSum):
+    for i in range(len(array) - 1):
+      firstNum = array[i]
+      for j in range(i + 1, len(array)):
+        secondNum = array[j]
+        if firstNum + secondNum == targetSum:
+          return [firstNum, secondNum]
+    return []
