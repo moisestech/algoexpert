e4bf2cdc999d0069832a2cd28e161a8f2c9a9aea
moisestech <m@moises.tech>
2021-06-07 05:19:40 -0400

    solution ts

--- Easy/minimum-waiting-time/solution_1/minimum-waiting-time.ts
+++ Easy/minimum-waiting-time/solution_1/minimum-waiting-time.ts
@@ -1,2 +1,9 @@
+// resolve TypeScript error - Cannot redeclare block-scoped variable.
+export {};
+
 // SOLUTION 1
 // Minimum Waiting Time
+
+// Complexity
+// Average:  Time: | Space:
+// Worst:    Time: | Space:
