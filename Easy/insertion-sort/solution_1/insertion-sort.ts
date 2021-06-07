85b306b0d8231b300b78f718768a42ca2c85adda
moisestech <m@moises.tech>
2021-06-07 05:20:30 -0400

    solution ts

--- Easy/insertion-sort/solution_1/insertion-sort.ts
+++ Easy/insertion-sort/solution_1/insertion-sort.ts
@@ -1,2 +1,9 @@
+// resolve TypeScript error - Cannot redeclare block-scoped variable.
+export {};
+
 // SOLUTION 1
 // Insertion Sort
+
+// Complexity
+// Average:  Time: | Space:
+// Worst:    Time: | Space:
