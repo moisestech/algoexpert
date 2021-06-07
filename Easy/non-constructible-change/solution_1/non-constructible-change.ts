96b806e1d6d497e320335648e58c2409a13ac23a
moisestech <m@moises.tech>
2021-06-07 05:17:40 -0400

    solution ts

--- Easy/non-constructible-change/solution_1/non-constructible-change.ts
+++ Easy/non-constructible-change/solution_1/non-constructible-change.ts
@@ -1,5 +1,13 @@
+// resolve TypeScript error - Cannot redeclare block-scoped variable.
+export {};
+
 // SOLUTION 1
 // Non-Costructible Change
+
+// Complexity
+// Average:  Time: | Space:
+// Worst:    Time: | Space:
+
 // O(n log n) time | O(1) space where n is the number of coins
 export function nonConstructibleChange(coins: number[]) {
   coins.sort((a, b) => a - b);
