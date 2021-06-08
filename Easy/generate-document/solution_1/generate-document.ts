10efa18c61add5dd8f75a207c848f8a12a8d680a
moisestech <m@moises.tech>
2021-06-08 09:23:57 -0400

    init ts

--- Easy/generate-document/solution_1/generate-document.ts
+++ Easy/generate-document/solution_1/generate-document.ts
@@ -1,2 +1,9 @@
+// resolve TypeScript error - Cannot redeclare block-scoped variable.
+export {};
+
 // SOLUTION 1
 // Generate Document
+
+// Complexity
+// Average:  Time: | Space:
+// Worst:    Time: | Space:
