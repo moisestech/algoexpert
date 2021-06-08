cee741f21e6440c719cdc957140658a6203f0b18
moisestech <m@moises.tech>
2021-06-08 09:24:24 -0400

    init ts

--- Easy/first-non-repeating-character/solution_1/first-non-repeating-character.ts
+++ Easy/first-non-repeating-character/solution_1/first-non-repeating-character.ts
@@ -1,2 +1,9 @@
+// resolve TypeScript error - Cannot redeclare block-scoped variable.
+export {};
+
 // SOLUTION 1
 // First Non-Repeating Characters
+
+// Complexity
+// Average:  Time: | Space:
+// Worst:    Time: | Space:
