d32d2946e2ed6e228e3c9af49baf214963e1641a
moisestech <m@moises.tech>
2021-06-08 09:27:58 -0400

    init ts

--- Easy/depth-first-search/solution_1/depth-first-search.ts
+++ Easy/depth-first-search/solution_1/depth-first-search.ts
@@ -1,2 +1,9 @@
+// resolve TypeScript error - Cannot redeclare block-scoped variable.
+export {};
+
 // SOLUTION 1
 // Depth First Search
+
+// Complexity
+// Average:  Time: | Space:
+// Worst:    Time: | Space:
