f858291f26939e0d6f07832fb02fd714b66213f0
moisestech <m@moises.tech>
2021-06-08 09:25:44 -0400

    init ts

--- Easy/find-three-largest-numbers/solution_1/find-three-largest-numbers.ts
+++ Easy/find-three-largest-numbers/solution_1/find-three-largest-numbers.ts
@@ -1,2 +1,9 @@
+// resolve TypeScript error - Cannot redeclare block-scoped variable.
+export {};
+
 // SOLUTION 1
 // Find Three Largest Numbers
+
+// Complexity
+// Average:  Time: | Space:
+// Worst:    Time: | Space:
