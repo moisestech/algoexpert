f338f66893b7d0906af50ac54fd3079e87bf7a44
moisestech <m@moises.tech>
2021-06-08 06:14:37 -0400

    init ts

--- Easy/node-depths/solution_1/node-depths.ts
+++ Easy/node-depths/solution_1/node-depths.ts
@@ -5,5 +5,25 @@ export {};
 // Node Depths
 
 // Complexity
-// Average:  Time: | Space:
-// Worst:    Time: | Space:
+// Average: when the tree is balanced
+// 	Time: O(n)  | n is the number of nodes in the Binary Tree
+// 	Space: O(h)	| h is the height of the Binary Tree
+// Worst: when the tree is not balanced
+
+export function nodeDepths(root: BinaryTree) {
+  // Write your code here.
+  return -1;
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
