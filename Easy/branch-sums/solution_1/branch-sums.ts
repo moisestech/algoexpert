6da5508658c57203986cc392f62eafb07ae7a59d
moisestech <m@moises.tech>
2021-06-10 05:45:41 -0400

    solution ts

--- Easy/branch-sums/solution_1/branch-sums.ts
+++ Easy/branch-sums/solution_1/branch-sums.ts
@@ -1,2 +1,26 @@
+// resolve TypeScript error - Cannot redeclare block-scoped variable.
+export {};
+
 // SOLUTION 1
 // Branch Sums
+
+// Complexity
+// Average:  Time: | Space:
+// Worst:    Time: | Space:
+
+class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function branchSums(root: BinaryTree): number[] {
+  // Write your code here.
+  return [-1];
+}
