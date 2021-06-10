8e7ca06e16237aadf9cc5e2f1baf35a5464e4c6e
moisestech <m@moises.tech>
2021-06-09 21:07:53 -0400

    notes solution

--- Easy/minimum-waiting-time/solution_1/minimum-waiting-time.ts
+++ Easy/minimum-waiting-time/solution_1/minimum-waiting-time.ts
@@ -7,3 +7,13 @@ export {};
 // Complexity
 // Average:  Time: | Space:
 // Worst:    Time: | Space:
+
+// positive integers in the input array / no floating point values
+// given at least one query / edge case
+// You can mutate the input array
+
+const inputArr = [1, 2, 2, 3, 6];
+// executing the shorted query first
+// you are making sure that the query after has the minimum waiting time
+// came up with a greedy algorithm
+// we make a greedy choice at each step
