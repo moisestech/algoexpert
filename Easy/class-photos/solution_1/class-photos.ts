0d4fae75eebf7a2435a3eef29b2b05dacf9e796d
moisestech <m@moises.tech>
2021-06-09 21:11:52 -0400

    init ts

--- Easy/class-photos/solution_1/class-photos.ts
+++ Easy/class-photos/solution_1/class-photos.ts
@@ -1,2 +1,9 @@
+// resolve TypeScript error - Cannot redeclare block-scoped variable.
+export {};
+
 // SOLUTION 1
 // Class Photos
+
+// Complexity
+// Average:  Time: | Space:
+// Worst:    Time: | Space:
