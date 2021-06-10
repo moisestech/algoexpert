50523c31315f9b8f8dc40b25b84caa1301f0a0ef
moisestech <m@moises.tech>
2021-06-10 05:46:41 -0400

    solution py

--- Easy/class-photos/solution_1/class-photos.py
+++ Easy/class-photos/solution_1/class-photos.py
@@ -2,5 +2,27 @@
 # Class Photos
 
 # Complexity
-# Average:  Time: | Space:
+# Average:  Time: O(nlog(n))  | Space: O(1)
 # Worst:    Time: | Space:
+# where n is the number of students
+
+def classPhotos(redShirtHeights, blueShirtHeights):
+
+  redShirtHeights.sort(reverse=True)
+  blueShirtHeights.sort(reserver=True)
+
+  shirtColorInFirstRow = "RED" if redShirtHeights[0] < blueShirtHeights[0] else "BLUE"
+
+  for idx in range(len(redShirtHeights)):
+    redShirtHeight = redShirtHeights[idx]
+    blueShirtHeight = blueShirtHeights[idx]
+
+    if shirtColorInFirstRow == "RED":
+      if redShirtHeight >= blueShirtHeight:
+        return False
+
+    else:
+      if blueShirtHeight >= redShirtHeight:
+        return False
+  
+  return False
