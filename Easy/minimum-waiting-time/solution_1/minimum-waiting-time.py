9e09472d1df9dc6779aa4121a42d6bce0eae8e35
moisestech <m@moises.tech>
2021-06-11 06:52:03 -0400

    solution py complete

--- Easy/minimum-waiting-time/solution_1/minimum-waiting-time.py
+++ Easy/minimum-waiting-time/solution_1/minimum-waiting-time.py
@@ -2,5 +2,24 @@
 # Minimum Waiting Time
 
 # Complexity
-# Average:  Time: | Space:
+# Average:  Time: O(n log n) | Space: O(1)
 # Worst:    Time: | Space:
+# n is the number of queries in the input array
+
+# Because we can mutate the array we can sort in place
+# saving the external space
+
+# 1. Sort the input array
+# total = 0
+# all querys need to wait the current index times the numbers left in the array
+# if it's the last query no need to wait)
+
+def minimumWaitingTime(queries):
+  queries.sortt()
+
+  totalWaitingTime = 0
+  for idx, duration in enumerate(queries):
+    queriesLeft = len(queries) - (idx + 1)
+    totalWaitingTime += duration * queriesLeft
+
+  return totalWaitingTime 
